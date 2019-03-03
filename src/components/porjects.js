75bab9a7b441524cfcf7540a9f2e3c3fe323a9d6
Yash Thakkar <thakkaryash94@gmail.com>
2019-03-03 16:16:00 +0530

    add projects-repo card for user projects

---
+++ src/components/porjects.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import RepoCard from "./repoCard"
+
+function Projects() {
+  const { github: { viewer: { repositories } } } = useStaticQuery(
+    graphql`
+      query {
+        github {
+          viewer {
+            repositories(privacy: PUBLIC, affiliations: OWNER, first: 9, orderBy: {field: CREATED_AT, direction: DESC}) {
+              nodes {
+                owner {
+                  login
+                }
+                name
+                url
+                description
+                stargazers {
+                  totalCount
+                }
+                forkCount
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+  return (
+    <>
+      <h2 className="text-white">My Projects</h2>
+      <p className="f4 mb-4 text-white ">GitHub repositories that I've built.</p>
+      <div className="d-sm-flex flex-wrap gutter-condensed mb-4">
+        {repositories.nodes.map((repository, i) => (
+          <div key={i} className="col-sm-6 col-md-12 col-lg-6 col-xl-4 mb-3">
+            <RepoCard repository={repository} />
+          </div>
+        ))}</div>
+    </>
+  )
+}
+
+export default Projects
